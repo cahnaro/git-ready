8cbde32b60bd0b9a2337d0498ea3efb7b993c48f
cahnaro <112457742+cahnaro@users.noreply.github.com>
2023-03-16 18:31:02 +0700

    Update ready-to-git.py

--- ready-to-git.py
+++ ready-to-git.py
@@ -1,7 +1,4 @@
 # Tulis Nama dan NIM kamu sebagai penanda kamu sudah siap menggunakan GIT
 print("Berikut nama-nama yang sudah clone, modify, commit dan push file ini:")
-print("1. Moh Noor Al Azam (04060302)")
-print("2. Achmad Musyaffa Taufiqi (043129023)")
-print("3. Bhaskara Anugrah Pramada (043129027)")
-print("4. Muhammad Rizky Rahmattullah (04319058)")
-print()
+print("1. Cahyo Darujati (0710097402)")
+print("2. ")
